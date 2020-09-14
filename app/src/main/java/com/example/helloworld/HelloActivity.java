e6615889ecb519ebb64406190d1762c8318b1fb2
Malak <b.malika@gmail.com>
2020-09-14 18:34:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/helloworld/HelloActivity.java
@@ -0,0 +1,21 @@
+package com.example.helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class HelloActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_hello);
+
+        TextView V = (TextView) findViewById(R.id.textView);
+        Intent i = getIntent();
+        V.setText("Hello, " + i.getStringExtra("personName"));
+    }
+}
